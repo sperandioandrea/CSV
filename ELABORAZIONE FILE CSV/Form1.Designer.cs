e328f034a343b1ea6fd46955fd611b86da33c38c
Sperandio.20200 <sperandio.adrea.studente@itispaleocapa.it>
2023-05-19 13:39:16 +0200

    Aggiungere i file di progetto.

---
+++ ELABORAZIONE FILE CSV/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace ELABORAZIONE_FILE_CSV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(189, 30);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(371, 435);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "FUNZIONE1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 490);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ELABORAZIONE FILE CSV/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ELABORAZIONE_FILE_CSV
+{
+    public partial class Form1 : Form
+    {
+        public string path;
+        public Form1()
+        {
+            InitializeComponent();
+
+            path = Path.GetFullPath("..\\..\\..\\CSV\\sperandio1.csv");
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //FUNZIONE 1: AGGIUNTA
+        public void Aggiunta(char a = ';')
+        {
+            string[] linea = File.ReadAllLines(path);
+            if (File.Exists(path))
+            {
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                    Random r = new Random();
+                    linea[0] += ";miovalore";
+                    sw.WriteLine(linea[0]);
+
+                    for (int i = 1; i < linea.Length; i++)
+                    {
+                        linea[i] += a + r.Next(10, 20).ToString();
+                        sw.WriteLine(linea[i]); 
+                    }
+                }
+            }
+        }
+        //BOTTONE FUNZIONE 1
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Aggiunta();
+        }
+    }
+}
