e7e632c253c954cfaa9d060905c674e9d9931b50
andrea <sperandio.andrea.studente@itispaleocapa.it>
2023-05-23 20:41:34 +0200

    funzione 2: ContaCampi

--- ELABORAZIONE FILE CSV/Form1.Designer.cs
+++ ELABORAZIONE FILE CSV/Form1.Designer.cs
@@ -30,34 +30,49 @@
         {
             this.listView1 = new System.Windows.Forms.ListView();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listView1
             // 
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(189, 30);
+            this.listView1.Location = new System.Drawing.Point(252, 37);
+            this.listView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(371, 435);
+            this.listView1.Size = new System.Drawing.Size(493, 534);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(36, 41);
+            this.button1.Location = new System.Drawing.Point(48, 82);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(112, 23);
+            this.button1.Size = new System.Drawing.Size(149, 28);
             this.button1.TabIndex = 1;
-            this.button1.Text = "FUNZIONE1";
+            this.button1.Text = "FUNZIONE 1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(48, 132);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(149, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "FUNZIONE 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(751, 490);
+            this.ClientSize = new System.Drawing.Size(1001, 603);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.listView1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -69,6 +84,7 @@
 
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- ELABORAZIONE FILE CSV/Form1.cs
+++ ELABORAZIONE FILE CSV/Form1.cs
@@ -6,7 +6,8 @@ namespace ELABORAZIONE_FILE_CSV
 {
     public partial class Form1 : Form
     {
-        public string path;
+        public string path; //nome del file
+        public int numerocampi;
         public Form1()
         {
             InitializeComponent();
@@ -22,19 +23,19 @@ namespace ELABORAZIONE_FILE_CSV
         //FUNZIONE 1: AGGIUNTA
         public void Aggiunta(char a = ';')
         {
-            string[] linea = File.ReadAllLines(path);
+            string[] record = File.ReadAllLines(path);
             if (File.Exists(path))
             {
                 using (StreamWriter sw = new StreamWriter(path))
                 {
                     Random r = new Random();
-                    linea[0] += ";miovalore";
-                    sw.WriteLine(linea[0]);
+                    record[0] += ";miovalore";
+                    sw.WriteLine(record[0]);
 
-                    for (int i = 1; i < linea.Length; i++)
+                    for (int i = 1; i < record.Length; i++)
                     {
-                        linea[i] += a + r.Next(10, 20).ToString();
-                        sw.WriteLine(linea[i]); 
+                        record[i] += a + r.Next(10, 20).ToString();
+                        sw.WriteLine(record[i]); 
                     }
                 }
             }
@@ -43,6 +44,19 @@ namespace ELABORAZIONE_FILE_CSV
         private void button1_Click(object sender, EventArgs e)
         {
             Aggiunta();
+        }
+
+        //FUNZIONE 2: CONTACAMPI
+        public int ContaCampi(char a = ";")
+        {
+            string[] record = File.ReadAllLines(path);
+            numerocampi = record[0].Split(a).Length;
+            return numerocampi;
+        }
+        //BOTTONE FUNZIONE 2
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ContaCampi();
         }
     }
 }
